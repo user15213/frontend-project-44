beebeb6fe7a9d4f6d78845333a9e7735e64db935
user15213 <resomakar@gmail.com>
2025-02-19 13:38:05 +0500

    Add src/cli.js for user interaction and update bin/brain-games.js to use it

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from "readline-sync";
+
+const welcomeUser = () => {
+  const name = readlineSync.question("What is your name? ");
+  console.log(`Hello, ${name}! Welcome to the Brain Games!`);
+};
+
+export default welcomeUser;
